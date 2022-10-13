f581a9c9fcf9cb39237fa5cee49403e84788b9d3
Satyamisme <satyamisme@gmail.com>
2022-10-13 20:10:09 +0300

    Update leechstart.sh

--- leechstart.sh
+++ leechstart.sh
@@ -5,7 +5,7 @@ sudo apt install python3 python3-pip
 echo yes | pip3 install -r requirements-cli.txt
 echo yes | pip3 install -r requirements.txt
 #cp config_sample.env config.env
-#echo yes | sudo docker container prune
+echo yes | sudo docker container prune
 #echo yes | sudo docker image prune -a
 sudo dockerd
 sudo docker build . -t mirror-bot
