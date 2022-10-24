8c2a21690190c21e262d945ee77f5933ddec23a8
Satyamisme <satyamisme@gmail.com>
2022-10-24 15:15:56 +0300

    Update leechstart.sh

--- leechstart.sh
+++ leechstart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+cd $(dirname $0)
 #sudo docker ps -aq | xargs sudo docker stop | xargs sudo docker rm
 sudo apt install python3 python3-pip
 echo yes | pip3 install -r requirements-cli.txt
